7c8f46e5618c69d0b55157ddc72223dee95387be
Low Saetern <saetern.low123@yahoo.com>
2022-12-25 20:57:20 -0700

    created schema database

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
  const mysql = require('mysql2');
  const cTable = require('console.table');
- require ('dotenv').config();
+ require('dotenv').config();
 
  const connection = mysql.createConnection({
      host: 'localhost',
